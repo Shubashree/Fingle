288a4d2ae300d4ed11d1e4b84be49c13c4ec95d6
Duncan Ogle <duncan.ogle@gmail.com>
2014-05-13 08:30:06 +0100

    Removed immersive mode

--- app/src/main/java/uk/co/duncanogle/fingle/app/MainActivity.java
+++ app/src/main/java/uk/co/duncanogle/fingle/app/MainActivity.java
@@ -4,7 +4,6 @@ package uk.co.duncanogle.fingle.app;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -272,20 +271,6 @@ public class MainActivity extends Activity {
             }
         }
         return result;
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            gameView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            );}
     }
 
     /**
